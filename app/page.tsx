692262138c71ee5dd64ea9ed9ae4a1477a3b3d00
tomeytran <tomtran310251@gmail.com>
2024-05-06 10:50:44 +0700

    push changes

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,11 @@ export default function Page() {
           <p
             className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana}`}
           >
-            <strong>Welcome to Acme!</strong> This is the example for the{' '}
+            <strong>Welcome to Acme!!</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought to you by Vercel
           </p>
           <Link
             href="/login"
